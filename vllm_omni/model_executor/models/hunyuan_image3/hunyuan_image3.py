59498dbef4104570205864c4e627d7529cee177a
fan2956 <zhoufan53@huawei.com>
2026-05-28 18:04:28 +0800

    [Bugfix] fallback cos sin error
    Signed-off-by: fan2956 <zhoufan53@huawei.com>

--- vllm_omni/model_executor/models/hunyuan_image3/hunyuan_image3.py
+++ vllm_omni/model_executor/models/hunyuan_image3/hunyuan_image3.py
@@ -1411,9 +1411,8 @@ class HunyuanImage3RotaryEmbedding(nn.Module):
         # Interleave: [y*θ₀, x*θ₁, y*θ₂, x*θ₃, ...]
         freqs = torch.stack([y_freqs, x_freqs], dim=-1).reshape(num_tokens, -1)
 
-        emb = torch.cat([freqs, freqs], dim=-1)
-        cos = emb.cos().to(dtype)
-        sin = emb.sin().to(dtype)
+        cos = freqs.cos().to(dtype)
+        sin = freqs.sin().to(dtype)
 
         query = query.view(num_tokens, -1, self.head_dim)
         key = key.view(num_tokens, -1, self.head_dim)
